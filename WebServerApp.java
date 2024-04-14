dfaa375c96024d4ee7f4e1ac9ef9663359135d17
harouni-ziriab <146368465+harouni-ziriab@users.noreply.github.com>
2024-04-14 17:01:36 +0200

    Update WebServerApp.java

--- WebServerApp.java
+++ WebServerApp.java
@@ -1,8 +1,6 @@
 public class WebServerApp {
     public static void main(String[] args) {
-        // Create an instance of WebServer with port 80
         WebServer server = new WebServer(80);
-        // Start the server
         server.run();
     }
 }
